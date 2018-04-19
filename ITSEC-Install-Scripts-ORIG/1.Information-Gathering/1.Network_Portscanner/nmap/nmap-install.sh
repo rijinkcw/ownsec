6b49f8580da83cd9227db4b1df9564ea541c2b1d
alphaaurigae <you@example.com>
2018-04-19 06:45:08 +0200

    init

--- ITSEC-Install-Scripts-ORIG/1.Information-Gathering/1.Network_Portscanner/nmap/nmap-install.sh
+++ ITSEC-Install-Scripts-ORIG/1.Information-Gathering/1.Network_Portscanner/nmap/nmap-install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-cd /opt/ownsec/ITSEC-Install-Scripts-ORIG/1.Information-Gathering/1.Network_Portscanner/nmap_install
+. /opt/ownsec/ITSEC-Install-Scripts-ORIG/001.functions/all-scripts.sh
+
+GITREPO=https://github.com/nmap/nmap.git
+BRANCH=master
+GITREPOROOT=/opt/ownsec/ITSEC-Install-Scripts-ORIG/1.Information-Gathering/1.Network_Portscanner/nmap/nmap_install
+GITCLONEDIR=/opt/ownsec/ITSEC-Install-Scripts-ORIG/1.Information-Gathering/1.Network_Portscanner/nmap
+
+GITCLONEFUNC
+
 ./nmap-install.sh
