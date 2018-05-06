687d47235993500eca580556230b7642fbe29aa6
a <6D62@protonmail.ch>
2018-05-06 06:15:55 +0200

    ...

--- 0.Initial/src/PT2/4.UsrEnv_Setup/6.Grub_Settings.sh
+++ 0.Initial/src/PT2/4.UsrEnv_Setup/6.Grub_Settings.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Copy Grub config and Update Grub
-sudo cp /opt/ownsec/0.Initial/settings-scripts/grub /etc/default/grub
+# sudo cp /opt/ownsec/0.Initial/settings-scripts/grub /etc/default/grub
+sudo sed -i -e 's#GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"#GRUB_CMDLINE_LINUX_DEFAULT=""# g' /etc/default/grub 
+
 sudo update-grub
 sudo updatedb
 sudo ldconfig
--- 0.Initial/src/PT2/4.UsrEnv_Setup/7.IPv6_onoff.sh
+++ 0.Initial/src/PT2/4.UsrEnv_Setup/7.IPv6_onoff.sh
@@ -3,6 +3,8 @@
 #echo "net.ipv6.conf.all.disable_ipv6 = 1" > 01-disable-ipv6.conf
 #sudo mv 01-disable-ipv6.conf /etc/sysctl.d/01-disable-ipv6.conf
 
+sudo sed -i -e 's#GRUB_CMDLINE_LINUX_DEFAULT="net.ifnames=0 biosdevname=0#GRUB_CMDLINE_LINUX_DEFAULT="# g' /etc/default/grub 
+
 sudo sed -i -e 's#GRUB_CMDLINE_LINUX_DEFAULT="#GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 #g' /etc/default/grub 
 
 #sudo cp /etc/default/grub_tmp2 /etc/default/grub
