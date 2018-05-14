98d9df3a90c09195281380632305e4c871938dfb
alphaaurigae <6D62@protonmail.ch>
2018-05-14 05:11:33 +0200

    update linking

--- ITSEC-Install-Scripts-ORIG/1.Information-Gathering/1.Network_Portscanner/nmap/nmap-install.sh
+++ ITSEC-Install-Scripts-ORIG/1.Information-Gathering/1.Network_Portscanner/nmap/nmap-install.sh
@@ -3,7 +3,7 @@
 . /opt/ownsec/ITSEC-Install-Scripts-ORIG/001.functions/all-scripts.sh
 
 GITREPO=https://github.com/alphaaurigae/nmap_install
-BRANCH=xenial
+BRANCH=18.04-bionic_beaver
 GITREPOROOT=/opt/ownsec/ITSEC-Install-Scripts-ORIG/1.Information-Gathering/1.Network_Portscanner/nmap/nmap_install
 GITCLONEDIR=/opt/ownsec/ITSEC-Install-Scripts-ORIG/1.Information-Gathering/1.Network_Portscanner/nmap
 
